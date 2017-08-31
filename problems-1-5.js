4f711f106686d35f732ed59778d1dbf375ff136d
Daniel Alvarez <alvarezd@kapsch.net>
2017-08-30 21:23:55 -0300

    Problem 1

---
+++ problems-1-5.js
@@ -0,0 +1,17 @@
+// JavaScript: The Good Parts
+// Problems
+
+
+// Problem 1
+// Write a function that takes an argument and returns that argument
+// identity(3) // 3
+
+function identity(val) {
+    return val;
+}
+
+var identity = function identity(x) {
+    return x;
+}
+
+//alert(identity(3));
