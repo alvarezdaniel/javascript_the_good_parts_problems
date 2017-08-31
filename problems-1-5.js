7008789998e51becc8575b000d4849850bc66a0f
Daniel Alvarez <alvarezd@kapsch.net>
2017-08-30 22:32:24 -0300

    Problem 2

--- problems-1-5.js
+++ problems-1-5.js
@@ -15,3 +15,35 @@ var identity = function identity(x) {
 }
 
 //alert(identity(3));
+
+// Problem 2
+// Write two binary functions, add and mul, that take two numbers
+// and return their sum and product
+// add(3, 4) // 7
+// mul(3, 4) // 12
+
+var add = function add(a, b) {
+  return a + b;
+}
+
+var mul = function mul(a, b) {
+  return a * b;
+}
+
+//alert(add(3, 4));
+//alert(mul(3, 4));
+
+function add(x, y) {
+  return x + y;
+}
+
+function mul(x, y) {
+  return x * y;
+}
+
+// Problem 3
+// Write a function that takes an argument and returns a function
+// that returns that argument
+
+idf = identityf(3);
+idf() // 3
